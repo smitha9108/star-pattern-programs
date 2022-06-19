7b1d62c3dba4d719c4c36ad9337e7a87af07f047
SMITHA BHAT M <97948417+smitha9108@users.noreply.github.com>
2022-06-19 20:56:04 +0530

    Create 16.cpp
     \  *  *  *  *  *  /
     *  \  *  *  *  /  *
     *  *  \  *  /  *  *
     *  *  *  \  *  *  *
     *  *  /  *  \  *  *
     *  /  *  *  *  \  *
     /  *  *  *  *  *  \

---
+++ 16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<=7;i++)
+    {
+        for(j=1;j<=7;j++)
+        {
+            if(j==i)
+                cout<<" \\ ";
+            else if(j==8-i)
+                cout<<" / ";
+            else
+                cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
