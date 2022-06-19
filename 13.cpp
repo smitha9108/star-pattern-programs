69ead65d9c767182706b7cdd298c33d22b429296
SMITHA BHAT M <97948417+smitha9108@users.noreply.github.com>
2022-06-19 20:42:49 +0530

    Create 13.cpp
              1
           2  3  2
        3  4  5  4  3
     4  5  6  7  6  5  4

---
+++ 13.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k=1;
+    for(i=1;i<=4;i++)
+    {
+        k=i;
+        for(j=1;j<=7;j++)
+        {
+            if(j>=5-i&&j<=3+i)
+            {
+                cout<<" "<<k<<" ";
+                    j<4?k++:k--;
+            }
+
+            else
+                    cout<<"   ";
+        }
+        cout<<endl;
+    }
+}
